b082eeee13170e88f90959a4e9ec62e2be51e4cd
TolIk <anatoliyyushkevich@gmail.com>
2022-10-13 21:18:19 +0300

    what's wrong with "if"?

--- test.php
+++ test.php
@@ -1,14 +1,4 @@
 <?php
-function I($string_1, $string_2) {
-    $counter = 0;
-    for($i = 0; $i <= strlen($string_1); $i++){
-        echo $string_1[$i] . $string_2[$i] . "\n";
-        if ($string_1[$i] != $string_2[$i]){
-            echo $string_1[$i] . $string_2[$i] . "\n";
-            $counter++;
-            }
-    return $counter;
-    }
-}
-
-echo I('1010', '0110');
+$a = 'adcfs';
+$a = substr_replace($a, '', 2, 1);
+echo $a;
